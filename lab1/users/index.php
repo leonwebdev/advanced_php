d578419cb585d2e186d5d1ed0c7a4b567ec1fde2
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-05-24 14:44:32 -0500

    index.php done

--- lab1/users/index.php
+++ lab1/users/index.php
@@ -17,3 +17,25 @@ Model::init($dbh);
 $user = new User();
 
 // var_dump($user);
+
+if (!empty($_GET['id'])) {
+    $results = $user->getOne($_GET['id']);
+} else {
+    $results = $user->getAll();
+}
+
+$response = [];
+
+if (!empty($results)) {
+    $response['status'] = 'ok';
+} else {
+    $response['status'] = 'fail';
+}
+
+$response['results'] = $results ?? [];
+$response['errors'] = $errors ?? [];
+
+$json = json_encode($response);
+
+header('Content-Type: application/json');
+echo $json;
