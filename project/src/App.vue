79333b9c9a5c7d0b60d926704598e15a06b4be5c
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-06-02 20:54:10 -0500

    create form init

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,20 +1,58 @@
 <template>
+    <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        User Detail
+                    </h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <CreateForm :user="user" />
+                </div>
+            </div>
+        </div>
+    </div>
     <h1 class="mb-5">Full CRUD</h1>
-    <button class="btn btn-success mb-5">Create</button>
+    <button
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        class="btn btn-success mb-5"
+        @click="createUser()"
+    >
+        Create
+    </button>
     <ListView :users="users" />
 </template>
 
 <script>
     import ListView from "./components/ListView.vue";
+    import CreateForm from "./components/CreateForm.vue";
 
     export default {
         name: "App",
         components: {
             ListView,
+            CreateForm,
         },
         methods: {
             showListView() {
                 // ReLoad Full List View
+            },
+            createUser() {
+                this.user = this.user_void;
             },
         },
         data() {
@@ -23,6 +61,11 @@
                 error: false,
                 users: {},
                 user: false,
+                user_void: {
+                    name: "",
+                    email: "",
+                    phone: "",
+                },
             };
         },
         mounted() {
--- project/src/components/CreateForm.vue
+++ project/src/components/CreateForm.vue
@@ -1,12 +1,50 @@
 <template>
-    <form>
-        <input type="text" />
+    <form novalidate>
+        <div class="mb-3">
+            <label for="name" class="form-label fw-bold">Name</label>
+            <input
+                type="text"
+                class="form-control"
+                id="name"
+                name="name"
+                v-model="local_user.name"
+            />
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label fw-bold">Email</label>
+            <input
+                type="text"
+                class="form-control"
+                id="email"
+                name="email"
+                v-model="local_user.email"
+            />
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label fw-bold">Phone</label>
+            <input
+                type="text"
+                class="form-control"
+                id="phone"
+                name="phone"
+                v-model="local_user.phone"
+            />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>``
     </form>
 </template>
 
 <script>
     export default {
         name: "CreateForm",
+        props: {
+            user: Object,
+        },
+        data() {
+            return {
+                local_user: this.user,
+            };
+        },
     };
 </script>
 
